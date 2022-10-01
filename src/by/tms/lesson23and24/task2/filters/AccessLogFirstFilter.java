4c8c100859edec7a73b16bd14b25526462ba7fd6
AnastasiaDavydovskaya <nastya_davydovskaya@list.ru>
2022-10-02 01:12:52 +0300

    HomeTasks lesson23and24, make 1 filter instead 2

--- src/by/tms/lesson23and24/task2/filters/AccessLogFirstFilter.java
+++ src/by/tms/lesson23and24/task2/filters/AccessLogFirstFilter.java
@@ -12,32 +12,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebFilter(displayName = "AccessLogFilter", urlPatterns = {"/course/information"})
+@WebFilter(displayName = "AccessLogFilter", urlPatterns = {"/course/information", "/list/students/result"})
 public class AccessLogFirstFilter extends HttpFilter {
+
     public static List<User> users = new ArrayList<>();
 
     @Override
-    public void init() throws ServletException {
-        users.add(new User("user@mail.ru", "user"));
-    }
-
-    @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String addressInput = request.getParameter("addressInput");
-        String passwordInput = request.getParameter("passwordInput");
-        String addressRegistration = request.getParameter("addressRegistration");
-        String passwordRegistration = request.getParameter("passwordRegistration");
+        if (new String(request.getRequestURL()).equals("/course/information")) {
+            String addressInput = request.getParameter("addressInput");
+            String passwordInput = request.getParameter("passwordInput");
+            String addressRegistration = request.getParameter("addressRegistration");
+            String passwordRegistration = request.getParameter("passwordRegistration");
 
-        if (addressRegistration != null && passwordRegistration != null) {
-            users.add(new User(addressRegistration, passwordRegistration));
-        } else if (addressInput != null && passwordInput != null) {
-            for (User user : users) {
-                if (!user.equals(new User(addressInput, passwordInput))) {
-                    getServletContext().getRequestDispatcher("/authorization/error").forward(request, response);
+            if ((addressRegistration == null && passwordRegistration == null) && (addressInput == null && passwordInput == null)) {
+                getServletContext().getRequestDispatcher("/authorization").forward(request, response);
+            } else if (addressRegistration != null && passwordRegistration != null) {
+                users.add(new User(addressRegistration, passwordRegistration));
+            } else if (addressInput != null && passwordInput != null) {
+                for (User user : users) {
+                    if (!user.equals(new User(addressInput, passwordInput))) {
+                        getServletContext().getRequestDispatcher("/authorization/error").forward(request, response);
+                    }
                 }
             }
+
         } else {
-            getServletContext().getRequestDispatcher("/authorization").forward(request, response);
+            if (!users.isEmpty()) {
+                getServletContext().getRequestDispatcher("/authorization").forward(request, response);
+            }
         }
 
         chain.doFilter(request, response);
