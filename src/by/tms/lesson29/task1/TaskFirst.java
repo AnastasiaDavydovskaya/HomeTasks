74858c2c81142b1da9ab57ad8e5e0257071543b4
AnastasiaDavydovskaya <nastya_davydovskaya@list.ru>
2022-10-19 17:51:03 +0300

    HomeTasks lesson29, add two util classes, use set for optimization and rewrite work with file txt

--- src/by/tms/lesson29/task1/TaskFirst.java
+++ src/by/tms/lesson29/task1/TaskFirst.java
@@ -1,47 +1,39 @@
 package by.tms.lesson29.task1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import by.tms.lesson29.task1.utils.GenerateDateUtil;
+
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class TaskFirst {
 
     public static void main(String[] args) {
         Map<String, Document> map = new HashMap();
-        List<String> list = new ArrayList<>();
+        Set<Object> set = new HashSet<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("resources/lesson29/contacts.txt"))) {
-            String s;
-            while (( s = reader.readLine()) != null) {
-                if (!map.containsKey(s)) {
-                    if(!list.contains(s)) {
-                        map.put(s, new Document(s,
-                                LocalDate.ofEpochDay(ThreadLocalRandom.current().nextLong(
-                                        LocalDate.of(1970, 1, 1).toEpochDay(),
-                                        LocalDate.of(2015, 12, 31).toEpochDay()))));
-                    }
+        try (Stream<String> lines = Files.lines(Paths.get("resources/lesson29/contacts.txt"))) {
+            lines.forEach(line -> {
+                if (!set.contains(line)) {
+                    set.add(line);
                 } else {
-                    list.add(s);
-                    for(String element: list) {
-                        if (map.containsKey(element)) {
-                            map.remove(s);
-                        }
-                    }
+                    set.remove(line);
                 }
-            }
+            });
 
+            set.stream()
+                    .forEach(element -> map.put((String) element,
+                            new Document((String) element, GenerateDateUtil.generateDate())));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for (Map.Entry<String, Document> entry : map.entrySet()) {
-            System.out.println("Ключ =  " + entry.getKey() + " Значение = " + entry.getValue().getDate());
-        }
+
+        System.out.println(map);
     }
 }
--- src/by/tms/lesson29/task2/TaskSecond.java
+++ src/by/tms/lesson29/task2/TaskSecond.java
@@ -1,29 +1,23 @@
 package by.tms.lesson29.task2;
 
+import by.tms.lesson29.task2.utils.GenerateNamesUtil;
+
 import java.util.*;
 
 public class TaskSecond {
 
     public static void main(String[] args) {
         List<User> listUsers = new ArrayList<>();
-        String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         for (int i = 0; i < 1000; i++) {
-            StringBuilder builder = new StringBuilder();
-            int length = new Random().nextInt(5)+5;
-            for(int j = 0; j < length; j++) {
-                builder.append(lexicon.charAt(new Random().nextInt(lexicon.length())));
-            }
-            listUsers.add(new User(builder.toString(), 1 + (int) (Math.random() * 100)));
+            listUsers.add(new User(GenerateNamesUtil.generateName(), 1 + (int) (Math.random() * 100)));
         }
 
         Set<User> setUsers = new HashSet<>(listUsers);
 
-        List<User> sortedUsers = setUsers.stream()
-                .sorted(Comparator.comparing(User::getAge).reversed()).toList();
-
-        for(int i = 0; i < 10; i++) {
-            System.out.println(sortedUsers.get(i));
-        }
+        setUsers.stream()
+                .sorted(Comparator.comparing(User::getAge).reversed())
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
